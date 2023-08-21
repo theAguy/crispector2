07f7d3170cd44d59fdc79487a5ef83f5680640ae
theAguy <guyassa@gmail.com>
2023-08-21 13:53:46 +0300

    # put everything in a dictionary called crispector2 and change the import of all scripts inside the project to their correct new location

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup
+
+requirements = ['Click>=7.0', 'matplotlib >= 3.1.2', 'seaborn>=0.9.0','pyyaml>=5.1.2', 'plotly>=4.3.0'
+                'numpy>=1.12.1', 'pandas>=0.24.2', 'biopython>=1.74', 'scipy>=1.2.1', 'statsmodels',
+                'jinja2']
+
+setup(
+    author="Guy Assa",
+    author_email='guyassa@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+
+    ],
+    description="CRISPECTOR2 - Genome Editing Analysis Tool, with allele extension",
+    entry_points={
+        'console_scripts': [
+            'crispector = crispector.cli:main',
+        ],
+    },
+    python_requires='>=3.7',
+    install_requires=requirements,
+    include_package_data=True,
+    keywords='crispector',
+    name='crispector',
+    package_dir={'crispector': 'crispector'},
+    packages=['crispector', 'crispector.algorithm', 'crispector.config', 'crispector.input_processing',
+              'crispector.modifications', 'crispector.report', 'crispector.utils'],
+    url='https://github.com/theAguy/crispector2',
+    version='2.0.0',
+    zip_safe=False,
+)
