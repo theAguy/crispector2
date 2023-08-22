eda27b0f1f5f9abd0426b1e24cf3a6ced07cb164
theAguy <guyassa@gmail.com>
2023-08-22 18:42:10 +0300

    # v2.0.4

--- crispector2/__init__.py
+++ crispector2/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Guy Assa"""
 __email__ = 'guyassa@gmail.com'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     packages=['crispector2', 'crispector2.algorithm', 'crispector2.allele', 'crispector2.config',
               'crispector2.input_processing', 'crispector2.modifications', 'crispector2.report', 'crispector2.utils'],
     url='https://github.com/theAguy/crispector2',
-    version='2.0.3',
+    version='2.0.4',
     zip_safe=False,
 )
